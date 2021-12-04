e2ea5d8be2335b8d501f6242c98924a5a933c506
Vaclav Synacek <evasek@gmail.com>
2021-12-04 10:42:08 +0100

    03 second half

--- 03/solution.lisp
+++ 03/solution.lisp
@@ -30,3 +30,56 @@
 (* *gamma* *epsilon*)
 
 
+(setf *test-data*
+  (list "00100"
+        "11110"
+        "10110"
+        "10111"
+        "10101"
+        "01111"
+        "00111"
+        "11100"
+        "10000"
+        "11001"
+        "00010"
+        "01010"))
+
+
+(defun second-half (vals comp i)
+  (let* ((current-digits (mapcar
+                           (lambda (rec)
+                             (elt rec i))
+                           vals))
+         (winner (if (funcall comp
+                       (count #\1 current-digits)
+                       (count #\0 current-digits))
+                     #\1
+                     #\0))
+         (new-vals (remove-if-not
+                     (lambda (rec)
+                       (eql winner
+                            (elt rec i)))
+                     vals)))
+    (if (= 1 (length new-vals))
+      (first new-vals)
+      (second-half new-vals comp (+ i 1)))))
+
+
+(second-half *test-data* #'>= 0)
+(second-half *test-data* #'< 0)
+
+
+(setf *oxygen*
+      (parse-integer
+       (second-half *input-numbers* #'>= 0)
+       :radix 2))
+
+(setf *co2*
+      (parse-integer
+       (second-half *input-numbers* #'< 0)
+       :radix 2))
+
+(* *oxygen* *co2*)
+
+
+
