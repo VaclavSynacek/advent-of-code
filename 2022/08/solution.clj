3c3924410958f9c9284abe0846e734bfc30ee62e
Vaclav Synacek <evasek+git@gmail.com>
2022-12-08 17:29:39 +0000

    08a

---
+++ 2022/08/solution.clj
@@ -0,0 +1,78 @@
+(def small (slurp "small.txt"))
+
+(def input (slurp "input.txt"))
+
+
+small
+
+(defn parse [in]
+  (->> in
+    (clojure.string/split-lines)
+    (map #(map (comp read-string str) %))))
+
+(parse small)
+
+
+(defn visible-in-line [line]
+  (->> line
+    (map-indexed
+      (fn [idx val]
+        [val (take (+ idx 1) (cons -1 line))]))
+    (map
+      (fn [[tree trees]]
+        (> tree (reduce max trees))))))
+
+; (visible-in-line [2 3 4 0 2])
+
+(defn visible-from-left [forest]
+  (->> forest
+    (map visible-in-line)))
+
+;(visible-from-left (parse small))
+
+(visible-from-left (parse small))
+
+(defn nth-column [matrix n]
+  (for [row matrix] (nth row n)))
+
+(defn rot90 [matrix]
+  (for [column (reverse (range (count (first matrix))))]
+    (nth-column matrix column)))
+
+(defn rot90-n-times [times matrix]
+  (nth 
+    (iterate rot90 matrix)
+    times))
+             
+(def all-rotations
+  (map #(identity [% (- 4 %)]) (range 4)))
+             
+(defn visible-from-all-sides [matrix]
+  (map
+    (fn [[r1 r2]]
+       (->> matrix
+            (rot90-n-times r1)
+            (visible-from-left)
+            (rot90-n-times r2)))
+    all-rotations))
+
+(visible-from-all-sides (parse small))
+
+(defn matrix-map [f m1 m2]
+  (map
+    (fn [r1 r2]
+      (map f r1 r2))
+    m1 m2))
+
+(defn visible-all-around [matrix]
+  (reduce (partial matrix-map #(or %1 %2)) (visible-from-all-sides matrix)))
+
+;; first half
+(->> input
+     parse
+     visible-all-around
+     flatten
+     (filter identity)
+     count)
+
+
