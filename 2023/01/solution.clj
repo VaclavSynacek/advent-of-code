b85d12199e49e67e276676b6540f3e228cfddcc1
Vaclav Synacek <evasek@gmail.com>
2023-12-11 19:46:17 +0100

    01 second star

--- 2023/01/solution.clj
+++ 2023/01/solution.clj
@@ -7,139 +7,70 @@
     (clojure.string/split-lines)))
 
 
-(defn print-it [universe]
-  (doseq [l universe]
-    (println l)))
-
-
-(print-it small)
-
-(print-it input)
-
-
-(defn empty-line? [line]
-  (every? #(= \. %) line))
-
-(defn expand-lines [input]
-  (remove nil? (mapcat
-                 (fn [line]
-                   [line
-                    (when (empty-line? line)
-                      line)])
-                 input)))
-
-(print-it (expand-lines small))
-
-
-(defn transpose [universe]
-  (apply map str universe))
-
-
-(print-it (transpose small))
-
-(defn expand-universe [universe]
-  (->> universe
-    expand-lines
-    transpose
-    expand-lines
-    transpose))
-
-
-(print-it (expand-universe small))
-
-
-(defn get-galaxies [universe]
-  (->> (map-indexed
-        (fn [i row]
-          (map-indexed
-            (fn [j ch]
-              (when (= ch \#)
-                [i j]))
-            row))
-        universe)
-     (apply concat)
-     (remove nil?)))
-
-(defn pairs [galaxies]
-  (for [a galaxies
-        b galaxies
-        :while (not= a b)]
-       [a b]))
-
-
-(pairs [:a :b :c :d :e])
-
-(pairs (get-galaxies small))
-
-(defn distance [[[x1 y1] [x2 y2]]]
-  (+
-    (abs (- x1 x2))
-    (abs (- y1 y2))))
-
-(distance [[0 0] [1 1]])
+(defn line->data [line]
+  (let
+    [numbers (->> line
+               (map int)
+               (keep #(when (<= 48 % 57)
+                        %))
+               (map char))
+     data (parse-long (str (first numbers) (last numbers)))]
+    data))
 
 
 (->> small
-  expand-universe
-  get-galaxies
-  pairs
-  (map distance)
-  (reduce +))
+     (map line->data)
+     (reduce +))
+  
+(->> input
+     (map line->data)
+     (reduce +))
+
+;; --------------------------------------------------
+
+
+(def words
+  {"one" 1
+   "two" 2
+   "three" 3
+   "four" 4
+   "five" 5
+   "six" 6
+   "seven" 7
+   "eight" 8
+   "nine" 9})
+
+(defn starts-with-word-number? [text]
+  (->> (map
+         (fn [[key value]]
+            (when (clojure.string/starts-with? text key)
+              value))
+         words)
+    (remove nil?)
+    first))
+
+(defn starts-with-digit? [text]
+  (when (<= 48 (int (first text)) 57)
+    (parse-long (str (first text)))))
+
+(defn to-number [text]
+  (or (starts-with-digit? text)
+    (starts-with-word-number? text)))
+
+
+(defn line->numbers [text]
+  (->> (for
+         [i (range 0 (count text))]
+         (to-number (subs text i)))
+    (remove nil?)))
+
+(defn line->data [text]
+  (let
+    [numbers (line->numbers text)]
+    (+
+      (* 10 (first numbers))
+      (last numbers))))
 
 (->> input
-  expand-universe
-  get-galaxies
-  pairs
-  (map distance)
-  (reduce +))
-
-
-;; -------------------------------------------------
-
-
-(get-galaxies small)
-
-(print-it small)
-
-(defn empty-lines [lines]
-  (->> (map-indexed
-        (fn [i line]
-          (when (empty-line? line)
-            i))
-        lines)
-     (remove nil?)))
-
-(defn between? [a b c]
-  (or
-    (< a b c)
-    (> a b c)))
-
-(let
-  [
-   ;input small
-   input input
-   ;expand-by 1
-   expand-by 999999 
-   rows (empty-lines input)
-   cols (->> input
-          transpose
-          empty-lines)
-   galaxies (get-galaxies input)
-   extended-distance
-    (fn [[[x1 y1] [x2 y2] :as all]]
-      (+
-        (distance all)
-        (->> rows
-          (filter #(between? x1 % x2))
-          count
-          (* expand-by))
-        (->> cols
-          (filter #(between? y1 % y2))
-          count
-          (* expand-by))))]
-  (->> galaxies
-    pairs
-    (map extended-distance)
-    (reduce +)))
-    
-
+     (map line->data)
+     (reduce +))
